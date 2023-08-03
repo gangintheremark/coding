c7ef01f789a8ade7dcf071fa9d24056beef0419f
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2023-08-03 20:50:04 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -7,37 +7,30 @@ class Main {
         Scanner s = new Scanner(System.in);
 
         int[] arr = new int[9]; // 아홉 난쟁이
+        int sum  = 0;
         for (int i = 0; i < 9; i++) {
             arr[i] = s.nextInt();
+            sum += arr[i];
         }
 
         // brute force
         Arrays.sort(arr); // 오름차순 배열
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = i+1; j < 9; j++) {
-                for (int k = j+1; k < 9; k++) {
-                    for (int l = k+1; l < 9; l++) {
-                        for (int m = l+1; m < 9; m++) {
-                            for (int n = m+1; n < 9; n++) {
-                                for (int o = n+1; o < 9; o++) {
-                                    if(arr[o] + arr[n] + arr[m] + arr[l] + arr[k] + arr[j] + arr[i] == 100){
-                                        System.out.println(arr[i]);
-                                        System.out.println(arr[j]);
-                                        System.out.println(arr[k]);
-                                        System.out.println(arr[l]);
-                                        System.out.println(arr[m]);
-                                        System.out.println(arr[n]);
-                                        System.out.println(arr[o]);
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-                    }
+        int x =0, y=0;
+        for(int a = 0; a<arr.length-1;a++){
+            for(int b = a+1; b<arr.length;b++){
+                if(sum - arr[a] - arr[b] == 100){
+                    x = arr[a];
+                    y = arr[b];
+                    break;
                 }
             }
         }
+        for(int i : arr) {
+            if(i == x || i == y)
+                continue;
+            System.out.println(i);
+        }
+
     }
 }
 

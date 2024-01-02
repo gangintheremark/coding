039f524ed592c985fa877d3928c23d7ed10270b9
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-01-02 22:35:58 +0900

    [D1] Title: 홀수만 더하기, Time: 97 ms, Memory: 18,420 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -1,18 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution {
+    public static int sum = 0, tmp;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            int sum = 0;
+            sum = 0;
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < 10; j++) {

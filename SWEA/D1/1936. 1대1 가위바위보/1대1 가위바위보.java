7645b5a7cf6f58ce80e05c2e91aefce6397a852b
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-01-02 22:28:17 +0900

    [D1] Title: 1대1 가위바위보, Time: 100 ms, Memory: 18,636 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -1,15 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Solution {
     public static void main(String[] args) throws IOException {
-
+ 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+ 
         byte a = Byte.parseByte(st.nextToken());
         byte b = Byte.parseByte(st.nextToken());
-
+ 
         if ((a == 1 && b == 3) || (a == 2 && b == 1) || (a == 3 && b == 2))
             System.out.println("A");
         else

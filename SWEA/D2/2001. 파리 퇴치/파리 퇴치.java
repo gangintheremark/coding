5ed8a88d0d59d511ec28bb678ee3dcef4c2be8e9
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-01 17:28:57 +0900

    [D2] Title: 파리 퇴치, Time: 102 ms, Memory: 18,340 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,46 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int N, M;
+	static int[][] board;
+	static int max;
 
-    public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
 
-        int T = s.nextInt();
+		for (int t = 1; t <= T; t++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            int n = s.nextInt();
-            int m = s.nextInt();
-            int[][] arr = new int[n][n];
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    arr[j][k] = s.nextInt();
-                }
-            }
+			max = 0;
+			board = new int[N + 1][N + 1];
+			for (int i = 1; i <= N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= N; j++)
+					board[i][j] = board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1]
+							+ Integer.parseInt(st.nextToken());
+			}
 
-            int max = 0;
+			for (int i = M; i <= N; i++)
+				for (int j = M; j <= N; j++)
+					max = Math.max(max, board[i][j] - board[i - M][j] - board[i][j - M] + board[i - M][j - M]);
 
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    int sum = 0;
-                    for (int l = j; l < n && l < m + j; l++) {
-                        for (int o = k; o < n && o < m + k; o++) {
-                            sum += arr[l][o];
-                        }
-                    }
-                    if (max < sum) {
-                        max = sum;
-                    }
-                }
-            }
+			sb.append("#" + t + " " + max + "\n");
+		}
+		System.out.println(sb);
+	}
 
-            System.out.println("#" + (i+1) + " " + max);
-        }
-    }
 }
-
-

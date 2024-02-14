d164ebc1ed33d6229e8f0345bb9f06bb9f9c5bd9
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-14 09:35:36 +0900

    [Silver I] Title: 보석 상자, Time: 316 ms, Memory: 34820 KB -BaekjoonHub

--- 백준/Silver/2792. 보석 상자/보석 상자.java
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -9,10 +9,11 @@ public class Main {
 	private static int[] arr;
 	private static int answer = Integer.MAX_VALUE;
 
-	// 학생이 보석을 num개 씩 가진다고 가정할 떄, 몇 명에게 나눠줄 수 있는지?
-	// N 보다 많으면 lt = mid + 1 , answer 구할 필요 없음
-	// N 보다 적으면 rt = mid - 1
-	
+	// 학생이 보석을 num개 씩 가진다고 가정할 떄, 몇 명에게 나눠줄 수 있는지 리턴
+	// N 보다 많으면 lt = mid + 1 => answer 구할 필요 없음
+	// N 보다 적으면 rt = mid - 1 => num - 0 이 질투심
+	// N 과 같으면 num - min (rest의 최소값) 이 질투심
+
 	public static int solution(int num) {
 		int index = 0, count = 0, rest = 0, min = 0;
 		while (index < M) {
@@ -22,7 +23,7 @@ public class Main {
 			// 보석이 남는 경우
 			if (rest != 0) {
 				count++;
-				min = Math.min(rest, min);
+				min = Math.min(rest, min); // 남는 보석 중 가장 작은 보석
 			}
 
 			index++;
@@ -51,6 +52,7 @@ public class Main {
 			if (arr[i] > max)
 				max = arr[i];
 		}
+
 		answer = max;
 		int lt = 1;
 		int rt = max;
@@ -64,7 +66,7 @@ public class Main {
 				rt = mid - 1;
 			}
 		}
-		System.out.println(answer);
+		System.out.println(lt);
 
 	}
 }

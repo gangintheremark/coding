cb25567ebc428a4f812259b22833d9e09a7fee1f
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-16 16:17:44 +0900

    [Gold IV] Title: 알파벳, Time: 920 ms, Memory: 12616 KB -BaekjoonHub

--- 백준/Gold IV/1987. 알파벳/알파벳.java
+++ 백준/Gold IV/1987. 알파벳/알파벳.java
@@ -32,7 +32,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
@@ -46,7 +46,6 @@ public class Main {
         }
 
         T.DFS(0, 0, 1);
-        bw.write(answer + "\n");
-        bw.close();
+        System.out.println(answer);
     }
 }

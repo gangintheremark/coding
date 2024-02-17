729088efdcea5dafea600645f3a375f8dbeda1d9
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-17 19:06:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 156 ms, Memory: 51396 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,47 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.text.Collator;
+import java.util.*;
 
-class Main {
-	static int N, K;
-	static int[] W;
-	static int[] V;
-	static int[][] dp; // dp[i][w] 무게가 w인 가방에서 i번째 물건까지 판단했을 때의 최대 가치
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
 
-	public int knapsack(int i, int k) {
-		if (i < 0)
-			return 0;
-		if (dp[i][k] == -1) {
-			if (W[i] > k)
-				dp[i][k] = knapsack(i - 1, k); // 이전 i값 저장
-			else {
-				dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
-			}
-		}
-		return dp[i][k];
-	}
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		Main M = new Main();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		dp = new int[N][K + 1];
-		for (int[] col : dp) {
-			Arrays.fill(col, -1);
-		}
-		W = new int[N];
-		V = new int[N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			W[i] = Integer.parseInt(st.nextToken());
-			V[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] W = new int[N + 1];
+        int[] V = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
+        }
 
-		}
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
 
-		System.out.println(M.knapsack(N - 1, K));
-	}
+                if (W[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else
+                    dp[i][j] = Math.max(dp[i - 1][j], V[i] + dp[i - 1][j - W[i]]);
+            }
+
+        }
+        System.out.println(dp[N][K]);
+    }
 }

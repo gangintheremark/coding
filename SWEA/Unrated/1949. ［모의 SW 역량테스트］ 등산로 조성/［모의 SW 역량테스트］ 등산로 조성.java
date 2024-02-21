1b45b2200414741b14fe798261e0d40b8eb57dfa
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-21 15:49:33 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 128 ms, Memory: 19,912 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {

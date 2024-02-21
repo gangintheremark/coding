248ba33869aedea16d81de9a50833d1ea93e191b
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-21 16:18:48 +0900

    [Gold V] Title: 암호 만들기, Time: 80 ms, Memory: 11932 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,56 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
 
-    static int L, C;
-    static String[] arr, pwd;
+	static int L, C;
+	static String[] arr, pwd;
+	static StringBuilder sb = new StringBuilder();
 
-    public boolean isValid() {
-        int a = 0;
-        int b = 0;
-        for(String x : pwd) {
-            if (x.equals("a")|| x.equals("e")|| x.equals("i") || x.equals("o") ||x.equals("u"))
-                a++;
-            else b++;
-        }
-        if (a >= 1 && b >= 2) return true;
+	public boolean isValid() {
+		int a = 0;
+		int b = 0;
+		for (String x : pwd) {
+			if (x.equals("a") || x.equals("e") || x.equals("i") || x.equals("o") || x.equals("u"))
+				a++;
+			else
+				b++;
+		}
+		if (a >= 1 && b >= 2)
+			return true;
 
-        return false;
-    }
+		return false;
+	}
 
-    public void DFS(int depth, int start) {
-        if (depth == L) {
-            if (isValid()) {
-                for (String x : pwd)
-                    System.out.print(x);
-                System.out.println();
-            }
-        } else {
-            for (int i = start; i < C; i++) {
-                pwd[depth] = arr[i];
-                DFS(depth + 1, i + 1);
+	public void DFS(int depth, int start) {
+		if (depth == L) {
+			if (isValid()) {
+				for (String x : pwd)
+					sb.append(x);
+				sb.append('\n');
+			}
+		} else {
+			for (int i = start; i < C; i++) {
+				pwd[depth] = arr[i];
+				DFS(depth + 1, i + 1);
 
-            }
-        }
+			}
+		}
 
-    }
+	}
 
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		Main T = new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        L = Integer.parseInt(st.nextToken()); // n
-        C = Integer.parseInt(st.nextToken());
-        arr = new String[C];
-        pwd = new String[L];
+		L = Integer.parseInt(st.nextToken()); // n
+		C = Integer.parseInt(st.nextToken());
+		arr = new String[C];
+		pwd = new String[L];
 
-        arr = br.readLine().split(" ");
-        Arrays.sort(arr);
-        T.DFS(0, 0);
-        bw.close();
-    }
+		arr = br.readLine().split(" ");
+		Arrays.sort(arr);
+		T.DFS(0, 0);
+		System.out.println(sb);
+	}
 }

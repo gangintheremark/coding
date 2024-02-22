6a5c9e36d5cb127fc04e948cacf7428cefbd010b
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-22 13:17:10 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 210 ms, Memory: 24,760 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -3,9 +3,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
+
 
 public class Solution {
 	static List<int[]> cores = new ArrayList<int[]>();
@@ -14,10 +14,10 @@ public class Solution {
 	static int[] dx = { 1, -1, 0, 0 };
 	static int[] dy = { 0, 0, 1, -1 };
 
+    // 해당 방향으로 전선을 이을 수 있는지 체크 후 길이 출력
 	public static int calcLen(int x, int y, int d) {
 		int len = 0;
-		int tmpx = x; 
-		int tmpy = y;
+		int tmpx = x, tmpy = y;
 		while (true) {
 			tmpx += dx[d];
 			tmpy += dy[d];
@@ -26,6 +26,7 @@ public class Solution {
 					len++;
 					visited[tmpx][tmpy] = true;
 				} else {
+                    // 중간에 끊겼다면 지금까지 방문표시한 것들 백트래킹
 					while(true) {
 						tmpx -= dx[d];
 						tmpy -= dy[d];
@@ -69,12 +70,13 @@ public class Solution {
 			if (len == -1) {
 				continue; // 이 방향은 안돼
 			} else {
+                // 전선을 포함한 경우
 				dfs(index + 1, count + 1, length + len);
 				backtrace(nx, ny, d);
 
 			}
-
 		}
+        // 전선을 포함하지 않은 경우
 		dfs(index + 1, count, length);
 	}
 

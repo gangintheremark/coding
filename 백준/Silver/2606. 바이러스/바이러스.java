5e699e8f9f396b61df36c1407425d87320eaea1c
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-02-26 16:43:05 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11680 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,52 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static StringTokenizer st;
-	private static StringBuilder sb = new StringBuilder();
-	private static int N, M;
-	private static int[][] map;
-	private static boolean[] virus;
-
-	public static int bfs(int v) {
-		Deque<Integer> q = new ArrayDeque<Integer>();
-		q.offer(v);
-		virus[v] = true;
-		int count = 0;
-
-		while (!q.isEmpty()) {
-			int now = q.poll();
-
-			for (int i = 1; i <= N; i++) {
-				if(!virus[i] && map[now][i] == 1) {
-					virus[i] = true;
-					q.offer(i);
-					count++;
-				}
-			}
-		}
-
-		return count;
-	}
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		map = new int[N + 1][N + 1];
-		virus = new boolean[N + 1];
-		for (int i = 0; i < M; i++) {
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		int[][] graph = new int[n + 1][n + 1];
+		boolean[] visited = new boolean[101];
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			map[a][b] = 1;
-			map[b][a] = 1;
-		}
-		
-	
-		System.out.println(bfs(1));
-	}
 
+			graph[a][b] = 1;
+			graph[b][a] = 1;
+		}
+
+		Queue<Integer> q = new ArrayDeque<>();
+		// 1번 사람이 소문을 올렸을 때,
+		q.offer(1);
+		visited[1] = true;
+		int count = 0;
+		while (!q.isEmpty()) {
+			int now = q.poll();
+
+			for (int i = 1; i <= n; i++) {
+				if (!visited[i] && graph[now][i] == 1) {
+					visited[i] = true;
+					count++;
+					q.offer(i);
+				}
+			}
+		}
+		System.out.println(count);
+
+	}
 }

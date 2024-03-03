f914423d2124a2d9dc68953531981d7c4b3974af
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-03 12:50:09 +0900

    [Gold IV] Title: 최단경로, Time: 740 ms, Memory: 115516 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,96 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Node implements Comparable<Node> {
+		int index;
+		int distance;
+
+		public Node(int index, int distance) {
+			super();
+			this.index = index;
+			this.distance = distance;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			if (this.distance < o.distance)
+				return -1;
+			return 1;
+		}
+	}
+
+	static int v, e, k;
+	static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+	static int[] d;
+	static boolean[] visited;
+
+	public static void dijkstra(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+
+		pq.offer(new Node(start, 0));
+		d[start] = 0;
+		while (!pq.isEmpty()) {
+			Node node = pq.poll();
+			int dist = node.distance;
+			int now = node.index;
+
+			if (d[now] < dist)
+				continue;
+
+			for (int i = 0; i < graph.get(now).size(); i++) {
+				int cost = d[now] + graph.get(now).get(i).distance;
+
+				if (cost < d[graph.get(now).get(i).index]) {
+					d[graph.get(now).get(i).index] = cost;
+					pq.offer(new Node(graph.get(now).get(i).index, cost));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+
+		k = Integer.parseInt(br.readLine());
+
+		d = new int[v + 1];
+
+		for (int i = 0; i <= v; i++)
+			graph.add(new ArrayList<Node>());
+
+		for (int i = 0; i < e; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			graph.get(Integer.parseInt(st.nextToken()))
+					.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+
+		Arrays.fill(d, Integer.MAX_VALUE);
+
+		dijkstra(k);
+
+		for (int i = 1; i <= v; i++) {
+			if (d[i] == Integer.MAX_VALUE)
+				sb.append("INF");
+			else
+				sb.append(d[i]);
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}

51409237121ef730917577e68162aca4c0b24aa2
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-05 13:39:20 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 17232 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -4,9 +4,9 @@ import java.util.*;
 public class Main {
 	// 다익스트라
 	static int n, m, x, result = 0;
-	static List<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
-	static int[][] go;
-	static int[] back;
+	static List<ArrayList<Node>> graphIn = new ArrayList<ArrayList<Node>>();
+	static List<ArrayList<Node>> graphOut = new ArrayList<ArrayList<Node>>();
+	static int[] go, back;
 
 	// go : 1번 ~ N번 노드가 X번 마을로 가는 데 최소 비용
 	// back : X번 마을에서 1~N번 마을로 가는 데 최소 비용
@@ -26,14 +26,14 @@ public class Main {
 		}
 	}
 
-	public static void dijkstra(int start, int[] d) {
+	public static void dijkstra(int start, int[] d, List<ArrayList<Node>> graph) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.offer(new Node(start, 0));
 		d[start] = 0;
 		while (!pq.isEmpty()) {
-			Node tmp = pq.poll();
-			int dist = tmp.dist;
-			int now = tmp.v;
+			Node node = pq.poll();
+			int dist = node.dist;
+			int now = node.v;
 
 			if (d[now] < dist)
 				continue;
@@ -43,7 +43,10 @@ public class Main {
 
 				if (cost < d[graph.get(now).get(i).v]) {
 					d[graph.get(now).get(i).v] = cost;
-					if(graph.get(now).get(i).v == x) break;
+					if (graph.get(now).get(i).v == x && start != x) {
+						go[start] = cost;
+						break;
+					}
 					pq.offer(graph.get(now).get(i));
 				}
 
@@ -60,35 +63,31 @@ public class Main {
 		x = Integer.parseInt(st.nextToken());
 
 		back = new int[n + 1];
-		go = new int[n + 1][n + 1];
+		go = new int[n + 1];
 
-		Arrays.fill(back, Integer.MAX_VALUE);
-		for (int i = 1; i <= n; i++)
-			Arrays.fill(go[i], Integer.MAX_VALUE);
+		for (int i = 0; i <= n; i++)
+			graphIn.add(new ArrayList<>());
 
-		for (int i = 0; i <= n; i++) {
-			graph.add(new ArrayList<>());
-		}
+		for (int i = 0; i <= n; i++)
+			graphOut.add(new ArrayList<>());
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			graph.get(Integer.parseInt(st.nextToken()))
-					.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			graphIn.get(a).add(new Node(b, c));
+			graphOut.get(b).add(new Node(a, c));
 		}
 
-		for (int i = 1; i <= n; i++) {
-			// i번 노드에서 모든 노드까지의 최소거리 구하기
-			if (i == x)
-				continue;
-			dijkstra(i, go[i]);
-		}
+		Arrays.fill(go, Integer.MAX_VALUE);
+		Arrays.fill(back, Integer.MAX_VALUE);
 
-		dijkstra(x, back);
+		dijkstra(x, go, graphOut);
+		dijkstra(x, back, graphIn);
 
-		for (int i = 1; i <= n; i++) {
-			if (go[i][x] != Integer.MAX_VALUE)
-				result = Math.max(result, go[i][x] + back[i]);
-		}
+		for (int i = 1; i <= n; i++)
+			result = Math.max(result, go[i] + back[i]);
 
 		System.out.println(result);
 	}

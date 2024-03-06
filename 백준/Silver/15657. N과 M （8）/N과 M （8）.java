4429b0d7a0969f0439bb1460608f637f114a5172
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-06 10:51:57 +0900

    [Silver III] Title: N과 M (8), Time: 124 ms, Memory: 17204 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int n, m;
+	static int[] numbers, arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void permutation(int start, int count) {
+		if (count == m) {
+			for (int num : numbers)
+				sb.append(num).append(' ');
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = start; i < n; i++) {
+			numbers[count] = arr[i];
+			permutation(i, count + 1);
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		numbers = new int[m];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(arr);
+		permutation(0, 0);
+		System.out.println(sb);
+	}
+
+}

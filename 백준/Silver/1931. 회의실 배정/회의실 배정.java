6e15291c3c69631e6d5b9a61de899af0be3bbff8
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-08 10:10:33 +0900

    [Silver I] Title: 회의실 배정, Time: 504 ms, Memory: 43928 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,50 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-class Time implements Comparable<Time> {
-    int st;
-    int et;
-
-    public Time(int st, int et){
-        this.st = st;
-        this.et = et;
-    }
-    @Override
-    public int compareTo(Time o) {
-        if (this.et == o.et) return this.st - o.st;
-        return this.et - o.et;
-    }
-}
 public class Main {
-    static int n, m;
-    public int solution(ArrayList<Time> arr) {
-        int answer = 0;
-        int endTime = 0;
-        Collections.sort(arr);
-        for(Time o : arr) {
-            if (o.st >= endTime ) {
-                answer++;
-                endTime = o.et;
-            }
-        }
-        return answer;
-    }
+	
+	static class Node implements Comparable<Node> {
+		int startT;
+		int endT;
+		public Node(int startT, int endT) {
+			this.startT = startT;
+			this.endT = endT;
+		}
+		@Override
+		public int compareTo(Node o) {
+			if(this.endT == o.endT)
+				return this.startT - o.startT;
+			return this.endT - o.endT;
+		}		
+	}
 
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        ArrayList<Time> arr = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		List<Node> list = new ArrayList<Main.Node>();
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		
+		Collections.sort(list);
+		
+		int endTime = 0;
+		int result = 0;
+		for(Node node : list) {
+			if(endTime <= node.startT) {
+				result++;
+				endTime = node.endT;
+			}
+		}
+		System.out.println(result);
 
-        n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr.add(new Time(a,b));
-        }
-        bw.write(T.solution(arr) + "\n");
-        bw.close();
-    }
+	}
 }

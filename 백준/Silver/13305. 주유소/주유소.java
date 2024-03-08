ea34f7a29910bb4a9cc12fd16d3c61757fdcdbf3
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-08 11:14:21 +0900

    [Silver III] Title: 주유소, Time: 268 ms, Memory: 32656 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,32 +8,32 @@ public class Main {
 		StringTokenizer st;
 
 		int n = Integer.parseInt(br.readLine());
-		int[] oil = new int[n - 1];
-		int[] distance = new int[n - 1];
+		long[] oil = new long[n - 1];
+		long[] distance = new long[n - 1];
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n - 1; i++)
-			distance[i] = Integer.parseInt(st.nextToken());
+			distance[i] = Long.parseLong(st.nextToken());
 
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n - 1; i++)
-			oil[i] = Integer.parseInt(st.nextToken());
+			oil[i] = Long.parseLong(st.nextToken());
 
 		int result = 0;
-		int dist = distance[0]; // 오일을 사고 이동할 거리
-		int now = 0; // 오일을 살 도시
+		long dist = distance[0]; // 오일을 사고 이동할 거리
+		long now = oil[0]; // 오일 가격
 		/* 다음 도시가 나보다 오일값이 크다면 */
 		for (int i = 1; i < n - 1; i++) {
-			if (oil[i] > oil[i - 1]) {
+			if (oil[i] > now) {
 				dist += distance[i];
 			} else {
-				result += oil[now] * dist;
+				result += now * dist;
 				dist = distance[i];
-				now = i;
+				now = oil[i];
 			}
 		}
 
-		result += oil[now] * dist;
+		result += now * dist;
 
 		System.out.println(result);
 	}

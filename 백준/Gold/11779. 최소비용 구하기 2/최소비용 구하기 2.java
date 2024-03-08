313603d3ffbc2d57e6a530a1dd352798f54a228d
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-08 09:36:20 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 596 ms, Memory: 57076 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int n, m, a, b;
+	static int count;
+	static int[] d, pre;
+	static List<Integer> list = new ArrayList<Integer>();
+	static List<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+
+	static class Node implements Comparable<Node> {
+		int index;
+		int dist;
+
+		public Node(int v, int cost) {
+			this.index = v;
+			this.dist = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.dist - o.dist;
+		}
+
+	}
+
+	public static void dijkstra(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(start, 0));
+		d[start] = 0;
+		while (!pq.isEmpty()) {
+			Node node = pq.poll();
+			int now = node.index;
+			int dist = node.dist;
+
+			if (dist > d[now])
+				continue;
+
+			// now와 인접한 노드 중 출발노드에서 now를 거쳐가는 것이 더 빠른경우
+			for (int i = 0; i < graph.get(now).size(); i++) {
+				int cost = d[now] + graph.get(now).get(i).dist;
+				if (cost < d[graph.get(now).get(i).index]) {
+					pre[graph.get(now).get(i).index] = now;
+					d[graph.get(now).get(i).index] = cost;
+					pq.offer(new Node(graph.get(now).get(i).index, cost));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+
+		d = new int[n + 1];
+		pre = new int[n + 1];
+		Arrays.fill(d, Integer.MAX_VALUE);
+		for (int i = 0; i <= n; i++)
+			graph.add(new ArrayList<>());
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			graph.get(Integer.parseInt(st.nextToken()))
+					.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+
+		st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+
+		dijkstra(a);
+
+		sb.append(d[b]).append('\n');
+		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(b);
+		while(pre[b] != 0) {
+			count += 1;
+			stack.push(pre[b]);
+			b = pre[b];
+		}
+		
+		sb.append(count+1).append('\n');
+		while(!stack.isEmpty())
+			sb.append(stack.pop()).append(' ');
+		
+		System.out.println(sb);
+	}
+}

6908e38f52f44c44f14c9ef16574009c4ae1a565
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-08 11:42:57 +0900

    [Silver II] Title: N과 M (9), Time: 776 ms, Memory: 39932 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int n, m;
+	static int[] numbers, arr;
+	static boolean[] selected;
+	static LinkedHashSet<String> list = new LinkedHashSet<>();
+
+
+	public static void permutation(int count) {
+		if (count == m) {
+			StringBuilder sb = new StringBuilder();
+			for (int num : numbers)
+				sb.append(num).append(' ');
+			list.add(sb.toString());
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (!selected[i]) {
+				selected[i] = true;
+				numbers[count] = arr[i];
+				permutation(count + 1);
+				selected[i] = false;
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		numbers = new int[m];
+		selected = new boolean[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(arr);
+		permutation(0);
+		list.forEach(System.out::println);
+	}
+
+}

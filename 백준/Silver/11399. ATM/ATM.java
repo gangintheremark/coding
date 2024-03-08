653767c0aad6773924b59fa97e3449494e673529
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-08 10:15:45 +0900

    [Silver IV] Title: ATM, Time: 252 ms, Memory: 18768 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static class Node implements Comparable<Node> {
+		int startT;
+		int endT;
+		public Node(int startT, int endT) {
+			this.startT = startT;
+			this.endT = endT;
+		}
+		@Override
+		public int compareTo(Node o) {
+			if(this.endT == o.endT)
+				return this.startT - o.startT;
+			return this.endT - o.endT;
+		}		
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<Integer>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) 
+			list.add(Integer.parseInt(st.nextToken()));
+		
+		Collections.sort(list);
+		
+		int[] time = new int[n];
+		int sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			sum += list.get(i);
+			time[i] = sum;
+		}
+		System.out.println(Arrays.stream(time).sum());
+
+	}
+}

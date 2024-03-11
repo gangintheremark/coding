d635593e7ecf8a9de4ac6e91406c16117a9322da
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-11 10:57:17 +0900

    [Gold III] Title: 동전 분배, Time: 212 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -5,7 +5,7 @@ public class Main {
 
 	static int n;
 	static int[][] coins;
-	static int[][] dp;
+	static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,24 +32,23 @@ public class Main {
 
 			// dp
 			int mid = sum / 2;
-			dp = new int[n + 1][mid + 1]; // i번째 동전으로 0부터 ~ mid 까지 만들 수 있는지
+			dp = new int[mid + 1]; // i번째 동전으로 0부터 ~ mid 까지 만들 수 있는지
 
-			for (int[] cols : dp)
-				Arrays.fill(cols, Integer.MAX_VALUE);
+			Arrays.fill(dp, Integer.MAX_VALUE);
 
-			dp[0][0] = 0;
+			dp[0] = 0;
 
 			for (int i = 0; i < n; i++) {
 				int[] coin = coins[i];
 				for (int j = 0; j <= mid; j++) {
-					if (dp[i][j] == Integer.MAX_VALUE)
+					if (dp[j] == Integer.MAX_VALUE)
 						continue;
-					if (j + coin[0] <= mid && dp[i][j] < coin[1])
-						dp[i][j + coin[0]] = Math.min(dp[i][j + coin[0]], dp[i][j] + 1);
-					dp[i + 1][j] = 0;
+					if (j + coin[0] <= mid && dp[j] < coin[1])
+						dp[j + coin[0]] = Math.min(dp[j + coin[0]], dp[j] + 1);
+					dp[j] = 0;
 				}
 			}
-			if (dp[n][mid] == 0)
+			if (dp[mid] == 0)
 				sb.append(1);
 			else
 				sb.append(0);

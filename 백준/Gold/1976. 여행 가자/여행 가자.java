521cc85b0f356b64e03fb1db9c67db8cb60cd0ee
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-14 20:20:31 +0900

    [Gold IV] Title: 여행 가자, Time: 148 ms, Memory: 16764 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // 크루스칼
+    static int n, m;
+    static int[] parent;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    static class Node {
+        int v;
+        int w;
+        int dist;
+
+        public Node(int v, int w, int dist) {
+            this.v = v;
+            this.w = w;
+            this.dist = dist;
+        }
+    }
+
+    public static int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (b > a) parent[b] = parent[a];
+        else parent[a] = parent[b];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        String result = "YES";
+        n = Integer.parseInt(br.readLine()); // 도시의 수
+        m = Integer.parseInt(br.readLine()); // 여행다닐 도시의 수
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+
+                if (tmp == 1)
+                    union(i, j);
+            }
+
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < m; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+
+            if (find(a) != find(tmp)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(result);
+    }
+
+}

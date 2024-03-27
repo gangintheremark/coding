9ddf3ce80b224a4e578eac88bee29fba12c8d806
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-03-27 21:51:23 +0900

    [level 3] Title: 불량 사용자, Time: 22.76 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -3,12 +3,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    static boolean[] checked; 
+    static boolean[] visited;
     static Set<String> set = new HashSet<>();
 
-    public static void dfs(int banId, String res, String[] banned_id, String[] user_id) {
+    public static void dfs(int banId, String combi, String[] banned_id, String[] user_id) {
         if (banId == banned_id.length) {
-            String[] arr = res.split(" ");
+            String[] arr = combi.split(" ");
             Arrays.sort(arr); // 조합의 중복여부 판단을 위해
 
             String str = "";
@@ -19,22 +19,22 @@ public class Solution {
         }
 
         for (int i = 0; i < user_id.length; i++) {
-            if (!checked[i] && user_id[i].matches(banned_id[banId])) {
-                checked[i] = true;
-                dfs(banId + 1, user_id[i] + " " + res, banned_id, user_id);
-                checked[i] = false;
+            if (!visited[i] && user_id[i].matches(banned_id[banId])) {
+                visited[i] = true;
+                dfs(banId + 1, combi + " " +  user_id[i], banned_id, user_id);
+                visited[i] = false;
             }
         }
     }
 
     public static int solution(String[] user_id, String[] banned_id) {
-        checked = new boolean[user_id.length];
+        visited = new boolean[user_id.length];
 
         // 특정 문자열을 탐색할 때, . 은 모든 문자로 대체될 수 있으므로 * 을 . 으로 변경
         for (int i = 0; i < banned_id.length; i++)
             banned_id[i] = banned_id[i].replace('*', '.');
 
-        dfs(0, "", banned_id, user_id);//조합
+        dfs(0, "", banned_id, user_id); // 조합
 
         return set.size();
     }

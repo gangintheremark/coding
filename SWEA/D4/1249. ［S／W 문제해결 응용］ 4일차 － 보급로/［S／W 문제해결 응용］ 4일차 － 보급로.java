eebc275e744834d2f05d8a5b02b097525b2e21d6
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-04-06 19:12:02 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 145 ms, Memory: 27,204 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+    static int N;
+	static int min;
+	static int[][] map;
+	static boolean[][] isVisited;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
+    
+	public static void main(String args[]) throws Exception
+	{
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+		
+		for(int tc=1; tc<=T; tc++) {
+			min = Integer.MAX_VALUE;
+			
+			N = Integer.parseInt(br.readLine());
+			map = new int[N][N];
+			isVisited = new boolean[N][N];
+			for(int i=0; i<N; i++) {
+				String s = br.readLine();
+				for(int j=0; j<N; j++) {
+					map[i][j] = s.charAt(j) - '0';
+				}
+			}
+			
+			bfs(0, 0);
+            sb.append('#').append(tc).append(' ').append(min).append('\n');
+		} 
+        System.out.println(sb);
+	}
+	
+	static void bfs(int x, int y) {
+		PriorityQueue<Pos> que = new PriorityQueue<>();
+		
+		que.add(new Pos(x, y, 0)); 
+		isVisited[x][y] = true; 
+		
+		while(!que.isEmpty()) {
+			Pos p = que.poll();
+			
+			int curX = p.x;
+			int curY = p.y;
+			int curT = p.time;
+			
+			if(curX == N-1 && curY == N-1) { 
+				min = min > curT ? curT : min;
+			}
+			
+			for(int t=0; t<4; t++) {
+				int nx = curX + dx[t];
+				int ny = curY + dy[t];
+				
+				if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+				if(!isVisited[nx][ny]) {
+					int totalTime = curT + map[nx][ny]; 
+					que.add(new Pos(nx, ny, totalTime)); 
+					isVisited[nx][ny] = true; 
+				}
+				
+			}
+			
+		}
+		
+	}
+    static class Pos implements Comparable<Pos>{
+		int x, y;
+		int time; // 복구하는 데 든 시간
+		
+		Pos(int x, int y, int time){
+			this.x = x;
+			this.y = y;
+			this.time = time;
+		}
+		
+		@Override
+		public int compareTo(Pos o) { 
+			if(this.time < o.time) {
+				return -1;
+			} else if(this.time > o.time) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+}

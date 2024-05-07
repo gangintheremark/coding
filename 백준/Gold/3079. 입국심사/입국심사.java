ec1bd591b92f1cf200de89295ac1c5a654ceb0f1
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-05-07 19:26:56 +0900

    [Gold V] Title: 입국심사, Time: 264 ms, Memory: 21488 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -8,20 +8,20 @@ public class Main {
     각 입국심사대마다 심사하는 시간이 다름
     바로 심사를 안받아도 됨
      */
-    static long n, m;
-    static Long[] time;
+    static int n, m;
+    static int[] time;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Long.parseLong(st.nextToken());
-        m = Long.parseLong(st.nextToken());
-        time = new Long[(int)n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        time = new int[n];
         long max = 0;
         long min = Long.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            time[i] = Long.parseLong(br.readLine());
+            time[i] = Integer.parseInt(br.readLine());
             max = Math.max(max, time[i]);
             min = Math.min(min, time[i]);
         }
@@ -29,18 +29,19 @@ public class Main {
         long left = min;
         long right = max * m;
         long mid = 0;
-        while (left <= right) {
+        loop : while (left <= right) {
             mid = (left + right) / 2;
 
             long count = 0;
             for (int i = 0; i < n; i++) {
                 count += mid / time[i];
-                
-                if(count > m ) break;
-            }
 
-            if (count >= m) right = mid - 1;
-            else left = mid + 1;
+                if(count >= m) {
+                    right = mid -1;
+                    continue loop;
+                }
+            }
+            left = mid + 1;
         }
         System.out.println(left);
     }

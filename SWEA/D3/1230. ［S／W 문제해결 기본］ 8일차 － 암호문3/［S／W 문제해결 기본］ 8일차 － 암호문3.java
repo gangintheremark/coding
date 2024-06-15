8fb0c58e64c678bb87dcfa5378a7667fd6d199d7
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-06-15 16:57:09 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 159 ms, Memory: 41,580 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -3,53 +3,155 @@ import java.io.*;
 
 public class Solution {
 
-    static ArrayList<Integer> list;
-    static StringTokenizer st;
+    static int NODE_MAX = 5000;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static class Node {
+        int data;
+        Node next;
 
-    private static void command(char cmd, int x) {
-        int y;
-        switch (cmd) {
-            case 'I':
-                y = Integer.parseInt(st.nextToken());
-                for (int i = 0, idx = x; i < y ; i++, idx++)
-                    list.add(idx, Integer.parseInt(st.nextToken()));
-                break;
-            case 'D':
-                y = Integer.parseInt(st.nextToken());
-                for (int i = 0; i < y; i++)
-                    list.remove(x);
-                break;
-            case 'A':
-                for (int i = 0; i < x; i++)
-                    list.add(Integer.parseInt(st.nextToken()));
-                break;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+
+    static class LinkedList {
+        Node head;
+        Node tail;
+        Node[] nodeArr;
+        int nodeCnt;
+
+        public LinkedList() {
+            head = null;
+            nodeArr = new Node[NODE_MAX];
+            nodeCnt = 0;
+        }
+
+        // data 를 값으로 갖는 새로운 node 생성
+        Node getNewNode(int data) {
+            nodeArr[nodeCnt] = new Node(data);
+            return nodeArr[nodeCnt++];
+        }
+
+        // idx개 이후 nums 배열 추가
+        void insert(int idx, int[] nums) {
+            int num = 0;
+            if(idx == 0) {
+                // 맨 앞에 추가하는 경우 head가 변경되어야 함 -> 맨 앞 데이터만 먼저 추가
+                Node newNode = getNewNode(nums[0]);
+                if(head != null) {
+                    newNode.next = head;
+                    head = newNode;
+                } else {
+                    head = newNode;
+                }
+                num = 1;
+                idx = 1;
+            }
+
+            Node cur = head;
+            // idx만큼 이동
+            for (int i = 1; i < idx ; i++)
+                cur = cur.next;
+
+            // nums 추가
+            for (int i = num; i < nums.length ; i++) {
+                Node newNode = getNewNode(nums[i]);
+                newNode.next = cur.next;
+                cur.next = newNode;
+                cur = newNode;
+            }
+
+            if(cur.next == null)
+                tail = cur;
+        }
+
+        // idx 부터 cnt 개 만큼 삭제
+        void delete(int idx, int cnt) {
+            Node cur = head;
+            if(idx == 0) {
+                // head부터 삭제되는 경우
+                for (int i = 0; i < cnt; i++)
+                    cur = cur.next;
+
+                head = cur;
+                return;
+            }
+
+            for (int i = 1; i < idx; i++)
+                cur = cur.next;
+
+            Node anchor = cur; // 삭제되기 직전 위치 기억하기
+            for (int i = 0; i < cnt; i++)
+                    cur = cur.next;
+
+            anchor.next = cur.next;
+            if (anchor.next == null)
+                tail = anchor;
+        }
+
+        void add(int data) {
+            Node cur = tail;
+            Node newNode = getNewNode(data);
+            cur.next = newNode;
+            tail = newNode;
+        }
+
+        void print() {
+            int cnt  =10;
+            Node cur = head;
+            while(--cnt >=0) {
+                sb.append(cur.data).append(' ');
+                cur = cur.next;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        list = new ArrayList<>();
+        StringTokenizer st;
         for (int tc = 1; tc <= 10; tc++) {
+            LinkedList list = new LinkedList();
+            sb.append('#').append(tc).append(' ');
+
             int n = Integer.parseInt(br.readLine());
+            int[] init = new int[n];
 
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < n; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
+                init[i] = Integer.parseInt(st.nextToken());
             }
+            list.insert(0, init);
 
             int cmdCount = Integer.parseInt(br.readLine());
-
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < cmdCount; i++) {
                 char cmd = st.nextToken().charAt(0);
-                int x = Integer.parseInt(st.nextToken());
-                command(cmd, x);
+               int x,y;
+               switch (cmd) {
+                   case 'I':
+                       x = Integer.parseInt(st.nextToken());
+                       y = Integer.parseInt(st.nextToken());
+                       int[] temp = new int[y];
+                       for (int j = 0; j < y; j++) {
+                           temp[j] = Integer.parseInt(st.nextToken());
+                       }
+                       list.insert(x, temp);
+                       break;
+                   case 'D' :
+                       x = Integer.parseInt(st.nextToken());
+                       y = Integer.parseInt(st.nextToken());
+                       list.delete(x, y);
+                       break;
+                   case 'A' :
+                       y = Integer.parseInt(st.nextToken());
+                       for (int j = 0; j < y; j++) {
+                           list.add(Integer.parseInt(st.nextToken()));
+                       }
+                       break;
+               }
             }
-
-            sb.append('#').append(tc);
-            for (int i = 0; i < 10; i++) {
-                sb.append(' ').append(list.get(i));
-            }
+            list.print();
             sb.append('\n');
         }
 

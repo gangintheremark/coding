f6a3a4335ff09582b6abedd5e76ae17e8da10e5a
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-06-17 14:42:32 +0900

    [Gold III] Title: 파티, Time: 140 ms, Memory: 17012 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,91 +1,88 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	// 다익스트라
-	static int n, m, x, result = 0;
-	static List<ArrayList<Node>> graphIn = new ArrayList<ArrayList<Node>>();
-	static List<ArrayList<Node>> graphOut = new ArrayList<ArrayList<Node>>();
-	static int[] go, back;
 
-	// go : 1번 ~ N번 노드가 X번 마을로 가는 데 최소 비용
-	// back : X번 마을에서 1~N번 마을로 가는 데 최소 비용
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+    private static int INF = Integer.MAX_VALUE;
+    private static int n, m;
+    private static int[] in, out;
+    private static ArrayList<ArrayList<Node>> graphIn = new ArrayList<>();
+    private static ArrayList<ArrayList<Node>> graphOut = new ArrayList<>();
 
-	static class Node implements Comparable<Node> {
-		int v;
-		int dist;
+    static class Node implements Comparable<Node> {
+        int index;
+        int cost;
 
-		public Node(int v, int cost) {
-			this.v = v;
-			this.dist = cost;
-		}
+        public Node(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
 
-		@Override
-		public int compareTo(Node o) {
-			return this.dist - o.dist;
-		}
-	}
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
 
-	public static void dijkstra(int[] d, List<ArrayList<Node>> graph) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(x, 0));
-		d[x] = 0;
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			int dist = node.dist;
-			int now = node.v;
+    public static void dijkstra(int start, int[] d, ArrayList<ArrayList<Node>> graph) {
+        d[start] = 0;
 
-			if (d[now] < dist)
-				continue;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
-			for (int i = 0; i < graph.get(now).size(); i++) {
-				int cost = d[now] + graph.get(now).get(i).dist;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.index;
 
-				if (cost < d[graph.get(now).get(i).v]) {
-					d[graph.get(now).get(i).v] = cost;
-					pq.offer(graph.get(now).get(i));
-				}
+            if (d[now] < node.cost) continue;
 
-			}
-		}
-	}
+            for (Node next : graph.get(now)) {
+                int cost = d[now] + next.cost;
+                if (d[next.index] > cost) {
+                    d[next.index] = cost;
+                    pq.offer(new Node(next.index, cost));
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		x = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= n; i++) {
+            graphIn.add(new ArrayList<Node>());
+            graphOut.add(new ArrayList<Node>());
+        }
 
-		back = new int[n + 1];
-		go = new int[n + 1];
+        in = new int[n + 1];
+        out = new int[n + 1];
+        Arrays.fill(in, INF);
+        Arrays.fill(out, INF);
 
-		for (int i = 0; i <= n; i++)
-			graphIn.add(new ArrayList<>());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i <= n; i++)
-			graphOut.add(new ArrayList<>());
+            graphIn.get(v).add(new Node(u, w));
+            graphOut.get(u).add(new Node(v, w));
+        }
 
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			graphIn.get(a).add(new Node(b, c));
-			graphOut.get(b).add(new Node(a, c));
-		}
+        dijkstra(start, in, graphIn);
+        dijkstra(start, out, graphOut);
 
-		Arrays.fill(go, Integer.MAX_VALUE);
-		Arrays.fill(back, Integer.MAX_VALUE);
-
-		dijkstra(go, graphOut);
-		dijkstra(back, graphIn);
-
-		for (int i = 1; i <= n; i++)
-			result = Math.max(result, go[i] + back[i]);
-
-		System.out.println(result);
-	}
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result = Math.max(result, in[i] + out[i]);
+        }
+        System.out.println(result);
+    }
 
 }

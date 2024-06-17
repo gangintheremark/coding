f8f103e1ebab1e65c9776a06704d556dc3d8a0a6
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-06-17 15:59:14 +0900

    [Gold IV] Title: 플로이드, Time: 356 ms, Memory: 43324 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,50 +1,55 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static final int INF = (int) 1e9;
-    static int n, m;
-    static int[][] graph;
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
+    private static int MAX_NODE = 101;
+    private static int INF = (int) 1e9; // 무한을 의미하는 값으로 10억 설정
+    private static int[][] graph = new int[MAX_NODE][MAX_NODE];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-
-        graph = new int[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++)
+        // 최단거리 테이블 초기화
+        for (int i = 0; i < MAX_NODE; i++) {
             Arrays.fill(graph[i], INF);
+        }
 
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
+        // 본인에서 본인으로 가는 비용은 0으로 초기화
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
                 if (i == j) graph[i][j] = 0;
+            }
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            graph[a][b] = Math.min(graph[a][b], c);
+            graph[u][v] = Math.min(graph[u][v], w); // 도시끼리 연결하는 노선은 하나가 아닐 수 있다
         }
 
-        for (int k = 1; k <= n; k++)
-            for (int i = 1; i <= n; i++)
-                for (int j = 1; j <= n; j++)
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
                     graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (graph[i][j] == INF) sb.append(0);
-                else  sb.append(graph[i][j]);
-                sb.append(' ');
+                sb.append(graph[i][j] == INF ? 0 : graph[i][j]).append(' ');
             }
             sb.append('\n');
         }
         System.out.println(sb);
     }
+
 }

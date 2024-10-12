d6b5e8643f32981ff0ff09f40df77c6e64733fbf
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-10-13 01:06:33 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 624 ms, Memory: 32028 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
+
 	static int n, m, r;
 	static int[][] map;
-	static int[] dx = { 0, 1, 0, -1 }; // 우 하 좌 상 
+	static int[] dx = { 0, 1, 0, -1 };
 	static int[] dy = { 1, 0, -1, 0 };
 
 	public static void main(String[] args) throws IOException {
@@ -19,6 +19,7 @@ public class Main {
 		r = Integer.parseInt(st.nextToken());
 
 		map = new int[n][m];
+
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
@@ -26,14 +27,14 @@ public class Main {
 			}
 		}
 
-		int point = (n > m ? m : n) / 2; // 회전시켜야 하는 그룹의 갯수
+		int point = (n > m ? m : n) / 2;
 
 		for (int i = 0; i < r; i++) {
 			rotate(point);
 		}
-		
-		for(int i=0; i< n;i++) {
-			for(int j=0;j<m;j++) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				sb.append(map[i][j]).append(' ');
 			}
 			sb.append('\n');
@@ -45,17 +46,16 @@ public class Main {
 		for (int t = 0; t < point; t++) {
 			int x = t;
 			int y = t;
-			
-			int tmp = map[x][y]; // 마지막에 넣을 값 빼놓기
-			
-			int d = 0; 
-			while(d < 4) {
-				// 왼쪽으로 넣는, 위로 넣는, 오른쪽으로 넣는, 아래로 넣는 연산 수행
+			int d = 0;
+
+			int tmp = map[x][y];
+
+			while (d < 4) {
 				int nx = x + dx[d];
 				int ny = y + dy[d];
-				
-				// 범위 안이라면
-				if(nx < n - t && ny < m - t && nx >= t && ny >= t) {
+
+				if (nx >= t && ny >= t && nx < n - t && ny < m - t) {
+
 					map[x][y] = map[nx][ny];
 					x = nx;
 					y = ny;
@@ -63,7 +63,7 @@ public class Main {
 					d++;
 				}
 			}
-			map[t+1][t] = tmp;
+			map[t + 1][t] = tmp;
 		}
 	}
 }

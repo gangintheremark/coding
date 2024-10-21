e35a020653d41f081240fec6a411ec1c1e271586
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-10-21 19:21:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringTokenizer st;
+	private static StringBuilder sb = new StringBuilder();
 
-		int num = N;
-		for (int x = 0; 3 * x <= N; x++) {
-			for (int y = 0; 3 * x + 5 * y <= N; y++) {
-				if (3 * x + 5 * y == N) {
-					if (x + y < num)
-						num = x + y;
-				}
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[n + 1];
+
+		if (n >= 3)
+			dp[3] = 1;
+		if (n >= 5)
+			dp[5] = 1;
+
+		for (int i = 6; i <= n; i++) {
+			if (i % 5 == 0) {
+				dp[i] = dp[i - 5] + 1;
+			} else if (i % 3 == 0) {
+				dp[i] = dp[i - 3] + 1;
+			} else if (dp[i - 5] != 0 && dp[i - 3] != 0) {
+				dp[i] = Math.min(dp[i - 5], i - 3) + 1;
 			}
 		}
-		
-		if(num == N)
-			System.out.println(-1);
-		else
-			System.out.println(num);
+
+		System.out.println(dp[n] == 0 ? -1 : dp[n]);
+
 	}
 }
